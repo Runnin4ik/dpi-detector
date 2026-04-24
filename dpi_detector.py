795f91f64832c83040bc77778a1176c64e1e9199
Kirill Minovsky <mr.kirilll12@mail.ru>
2026-04-24 15:12:58 +0300

    bump version to 3.2.0

--- dpi_detector.py
+++ dpi_detector.py
@@ -29,7 +29,7 @@ from core.dns_scanner import (
 )
 from utils.files import load_domains, load_tcp_targets, load_whitelist_sni, get_base_dir
 
-CURRENT_VERSION = "3.1.0"
+CURRENT_VERSION = "3.2.0"
 GITHUB_REPO     = "Runnin4ik/dpi-detector"
 
 DOMAINS         = load_domains()
